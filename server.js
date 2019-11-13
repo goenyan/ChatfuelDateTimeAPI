e4d898d2f90f07971daa82aa41a171dd4c1fdeb4
Glitch (hello-express) <none>
2019-11-13 13:17:14 +0000

    🐴🎋 Checkpoint ./package.json:318592/1530 ./server.js:318592/1692

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-const webviewRoutes = require("./routes/webviews");
+const webviewRoutes = require('./routes/webviews');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set("view engine", "pug");
-app.set("views", "./views");
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 // GET endpoint
-app.get("/", function(request, response) {
-  response.send("OK");
+app.get('/', function(request, response) {
+    response.send('OK');
 });
 
-app.post("/attributes/reset", (request, response) => {
-  const keyNames = Object.keys(request.body);
+app.post('/attributes/reset', (request, response) => {
+    const keyNames = Object.keys(request.body);
 
-  let attributes = {};
-  keyNames.forEach(keyName => {
-    attributes[keyName] = null;
-  });
+    let attributes = {};
+    keyNames.forEach(keyName => {
+        attributes[keyName] = null;
+    });
 
-  response.json({ set_attributes: attributes });
+    response.json({ set_attributes: attributes });
 });
 
-app.use("/webviews", webviewRoutes);
+app.use('/webviews', webviewRoutes);
 
 // Start the server and listen for incoming requests
 const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
