ef0d4bb5c3e1e5c5d46a3093f18cf19bd33af1af
Glitch (hello-express) <none>
2019-11-24 13:52:59 +0000

    🐾😘 Checkpoint ./server.js:318592/78 ./routes/verification.js:318592/286

--- routes/verification.js
+++ routes/verification.js
@@ -4,6 +4,17 @@ const router = express.Router();
 
 // Verify an email address
 router.get('/email', (request, response) => {
+    const {email} = request.query;
+  
+    const checkValidMxRecord(email)
+      .then(emailData => {
+        response.json({});
+      
+    })
+    .catch(error => {
+      response.json({});
+    });
+  
     // 1. Simple check if it's an email - has it got an @ sign in it?
 
     // 2. Perform an MX lookup - check if the DNS record exists
@@ -13,9 +24,6 @@ router.get('/email', (request, response) => {
     // 4. If we get a response, it's valid
 
     // 5. Set some Chatfuel attributes
-    response.json({
-        set_attributes: {},
-    });
 });
 
 module.exports = router;
