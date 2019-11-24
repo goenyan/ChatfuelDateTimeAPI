a26a4e0b3c188eef7698556db47437952a568038
Glitch (hello-express) <none>
2019-11-24 14:17:00 +0000

    📟🍤 Checkpoint ./routes/verification.js:318592/2098

--- routes/verification.js
+++ routes/verification.js
@@ -1,55 +1,27 @@
-const dns = require('dns');
 const express = require('express');
 
 const router = express.Router();
 
+// Simple check if it's an email - has it got an @ sign in it?
 const isValidEmailAddress = email => {
-  return new Promise((resolve, reject) => {
-    const isEmail = email.includes('@');
-    if (isEmail) {
-      resolve();
-    } else {
-      reject(new Error('Invalid email address as it does not contain an @ symbol'));
-    }
-  })
 };
 
+// Check if the lookup response contains ENOFOUND or ENODATA - if so, it's not valid
+const hasMxRecordError = error => {
+};
+
+// Perform an MX lookup - check if the DNS record exists
 const findMxRecords = email => {
-  const [,domain] = email.split('@');
-  
-  return dns.resolveMx
+  // Find the domain name from the email
+
+  // If we get a valid MX record response, it's a valid email
 };
 
 // Verify an email address
 router.get('/email', (request, response) => {
-    // Get email from the request query parameters
-    const {email} = request.query;
-  
-    // Simple check if it's an email - has it got an @ sign in it?
-    isValidEmailAddress(email)
-      .then(() => findMxRecords(email))
-      .then(emailMxRecords => {
-        response.json({
-          set_attributes: {
-            emailValid: true
-          }
-        });
-      })
-      .catch(error => {
-        response.json({
-          set_attributes: {
-            emailValid: false
-          }
-        });
-    });
+  // Get email from the request query parameters
 
-    // Perform an MX lookup - check if the DNS record exists
-
-    // Check if the lookup response contains ENOFOUND or ENODATA - if so, it's not valid
-
-    // If we get a response, it's valid
-
-    // Set some Chatfuel attributes
+  // Set some Chatfuel attributes
 });
 
 module.exports = router;
