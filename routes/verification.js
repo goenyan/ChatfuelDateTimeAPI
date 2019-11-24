ee7d87c96b6a54bfa94bf941a747e6ee2cc99089
Glitch (hello-express) <none>
2019-11-24 14:04:59 +0000

    ⏲🏨 Checkpoint ./routes/verification.js:318592/1695

--- routes/verification.js
+++ routes/verification.js
@@ -1,29 +1,55 @@
+const dns = require('dns');
 const express = require('express');
 
 const router = express.Router();
 
+const isValidEmailAddress = email => {
+  return new Promise((resolve, reject) => {
+    const isEmail = email.includes('@');
+    if (isEmail) {
+      resolve();
+    } else {
+      reject(new Error('Invalid email address as it does not contain an @ symbol'));
+    }
+  })
+};
+
+const findMxRecords = email => {
+  const [,domain] = email.split('@');
+  
+  return dns.resolveMx
+};
+
 // Verify an email address
 router.get('/email', (request, response) => {
+    // Get email from the request query parameters
     const {email} = request.query;
   
-    const checkValidMxRecord(email)
-      .then(emailData => {
-        response.json({});
-      
-    })
-    .catch(error => {
-      response.json({});
+    // Simple check if it's an email - has it got an @ sign in it?
+    isValidEmailAddress(email)
+      .then(() => findMxRecords(email))
+      .then(emailMxRecords => {
+        response.json({
+          set_attributes: {
+            emailValid: true
+          }
+        });
+      })
+      .catch(error => {
+        response.json({
+          set_attributes: {
+            emailValid: false
+          }
+        });
     });
-  
-    // 1. Simple check if it's an email - has it got an @ sign in it?
 
-    // 2. Perform an MX lookup - check if the DNS record exists
+    // Perform an MX lookup - check if the DNS record exists
 
-    // 3. Check if the lookup response contains ENOFOUND or ENODATA - if so, it's not valid
+    // Check if the lookup response contains ENOFOUND or ENODATA - if so, it's not valid
 
-    // 4. If we get a response, it's valid
+    // If we get a response, it's valid
 
-    // 5. Set some Chatfuel attributes
+    // Set some Chatfuel attributes
 });
 
 module.exports = router;
